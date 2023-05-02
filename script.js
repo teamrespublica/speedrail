d28d8082043e7571aab1a5a79a5a3cf138c0a65b
teamrespublica <130422094+teamrespublica@users.noreply.github.com>
2023-05-02 18:40:38 +0530

    Code added to remove Tableau Toolbar

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function checkTableauLoaded() {
+  if (typeof tableau !== 'undefined') {
+    var viz = tableau.VizManager.getVizs()[0];
+    if (viz.getWorkbook().getIsDoneRendering()) {
+      // The embedded code has fully loaded, hide the toolbar
+      var toolbar = document.querySelector('.tab-toolbar.public.tab-fill.tab-widget');
+      toolbar.style.display = 'none';
+    } else {
+      // The embedded code is still loading, wait and try again
+      setTimeout(checkTableauLoaded, 100);
+    }
+  } else {
+    // The Tableau JavaScript API is not yet available, wait and try again
+    setTimeout(checkTableauLoaded, 100);
+  }
+}
 
+checkTableauLoaded();
